b10991e7275da67f56fddcee235af2466042ae59
vatsalsharma376 <vatsalsharma376@gmail.com>
2022-09-11 19:05:00 +0530

    added path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import path from "path";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
